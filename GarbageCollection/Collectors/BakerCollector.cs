35268f419319cdd8b54294c61d4afc2b81237c93
Pascal Charpentier <charpentier_pasc@hotmail.com>
2025-04-05 14:22:04 -0400

    Test Baker initial ok.  Environnement de test fait.

--- GarbageCollection/Collectors/BakerCollector.cs
+++ GarbageCollection/Collectors/BakerCollector.cs
@@ -1,36 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System ;
+using System.Collections.Generic ;
+using System.Linq ;
+using System.Text ;
+using System.Threading.Tasks ;
 
 namespace GarbageCollection.Collectors
 {
     public class BakerCollector : IGarbageCollector
     {
-        public string Name => "Baker";
-        public static int NbPartitions => 1;
+        public string Name => "Baker" ;
+        public static int NbPartitions => 1 ;
 
-        private readonly EnvironmentMemory _memory;
+        private readonly EnvironmentMemory _memory ;
         private Mutator _mutator ;
 
         private HashSet<int> _free ;
         private HashSet<int> _unreached ;
-        
+
+        private void OnAdded(int address)
+        {
+            _unreached.Add(address) ;
+        }
 
         public BakerCollector(EnvironmentMemory memory, Mutator mutator)
         {
-            _memory = memory;
-            _mutator = mutator;
+            _memory = memory ;
+            _mutator = mutator ;
+            _mutator.Added += OnAdded ;
+
+            _unreached = new HashSet<int>() ;
             _free = new HashSet<int>() ;
-            _unreached  = new HashSet<int>(memory.RootReferences) ;
         }
 
         public bool ShouldCollect()
         {
             /* ------- À COMPLÉTER ------- */
 
-            return false;
+            return false ;
+        }
+
+        private HashSet<int> BuildUnscanned()
+        {
+            HashSet<int> unscanned = new HashSet<int>() ;
+            foreach (var r in _memory.RootReferences)
+            {
+                _unreached.Remove(r) ;
+                unscanned.Add(r) ;
+            }
+
+            return unscanned ;
         }
 
         private CollectableObject GetObjectFromAddress(int current)
@@ -39,41 +57,44 @@ namespace GarbageCollection.Collectors
             {
                 throw new Exception($"Unable to dereference {current} in GetObjectFromAddress.") ;
             }
+
             return obj ;
         }
 
-        private void FreeMemory()
+        private void ReleaseMemory()
         {
             foreach (var r in _free)
             {
-               _memory.Free(r) ; 
+                _memory.Free(r) ;
             }
+
             _free.Clear() ;
         }
 
         public void Collect()
         {
-           HashSet<int> unscanned = new HashSet<int>(_unreached); ;
-           HashSet<int> scanned  = new HashSet<int>() ;
-           
-           while (unscanned.Count > 0)
-           {
-               var current = unscanned.First() ;
-               unscanned.Remove(current) ;
-               scanned.Add(current) ;
-               
-               foreach (var r in GetObjectFromAddress(current).References)
-               {
-                   if (_unreached.Contains(r))
-                   {
-                       _unreached.Remove(r) ;
-                       unscanned.Add(r) ; 
-                   }
-               } 
-           }
-           _free.UnionWith(_unreached) ;
-           _unreached  = scanned ;
-           FreeMemory() ;
+            var unscanned = BuildUnscanned() ;
+            HashSet<int> scanned = new HashSet<int>() ;
+
+            while (unscanned.Count > 0)
+            {
+                var current = unscanned.First() ;
+                unscanned.Remove(current) ;
+                scanned.Add(current) ;
+
+                foreach (var r in GetObjectFromAddress(current).References)
+                {
+                    if (_unreached.Contains(r))
+                    {
+                        _unreached.Remove(r) ;
+                        unscanned.Add(r) ;
+                    }
+                }
+            }
+
+            _free.UnionWith(_unreached) ;
+            _unreached = new HashSet<int>(scanned) ;
+            ReleaseMemory() ;
         }
     }
 }
---
+++ GarbageCollectionTests/CollectorTestsEnvironment.cs
@@ -0,0 +1,74 @@
+using GarbageCollection ;
+using GarbageCollection.Collectors ;
+
+namespace GarbageCollectionTests ;
+
+public class CollectorTestsEnvironment
+{
+    protected BakerCollector _collector ;
+    protected EnvironmentMemory _memory ;
+    protected Mutator _mutator ;
+    protected CollectableObject A, B, C, D, E, F, G, H ;
+
+    protected int _lastAddedAddress = 301 ;
+    protected List<CollectableObject> _released = [] ;
+    protected Dictionary<CollectableObject, int> _objects = new([]) ;
+
+    protected void OnAdded(int address)
+    {
+        _lastAddedAddress = address ;
+    }
+
+    protected void OnRelease(CollectableObject obj)
+    {
+        _released.Add(obj) ;
+    }
+
+    protected void AddObjectToMemory(CollectableObject obj)
+    {
+        _memory.Add(obj) ;
+        _objects.Add(obj, _lastAddedAddress) ;
+        
+    }
+
+    protected void AddReferencesFromObjectTo(CollectableObject source, List<CollectableObject> destList)
+    {
+        var destinations = (from obj in destList select _objects[obj]).ToList (); ;
+        source.AddReferences(destinations) ;
+    }
+
+    protected void AddRootReferencesToMemory(List<CollectableObject> destList)
+    {
+        var destinations = (from obj in destList select _objects[obj]).ToList () ;
+        _memory.AddRootReferences(destinations) ;
+    }
+    
+    public void Setup()
+    {
+        _mutator = new Mutator() ;
+        _mutator.Added += OnAdded ;
+        _mutator.Released += OnRelease ;
+        _memory = new EnvironmentMemory(500, 1, _mutator) ;
+        _collector = new BakerCollector(_memory, _mutator) ;
+
+        A = new CollectableObject("A", 10, _mutator) ; 
+        B = new CollectableObject("B", 20, _mutator) ;
+        C = new CollectableObject("C", 30, _mutator) ;
+        D = new CollectableObject("D", 40, _mutator) ;
+        E = new CollectableObject("E", 50, _mutator) ;
+        F = new CollectableObject("F", 60, _mutator) ;
+        G = new CollectableObject("G", 70, _mutator) ;
+        H = new CollectableObject("H", 80, _mutator) ;
+
+        AddObjectToMemory(A) ;
+        AddObjectToMemory(B) ;
+        AddObjectToMemory(C) ;
+        AddObjectToMemory(D) ;
+        AddObjectToMemory(E) ;
+        AddObjectToMemory(F) ;
+        AddObjectToMemory(G) ;
+        AddObjectToMemory(H) ;
+        
+    }
+
+}
