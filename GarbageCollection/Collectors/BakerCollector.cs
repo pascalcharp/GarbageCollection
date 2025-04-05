b3422267f59058144a9ad081056b1cb007c20103
Pascal Charpentier <charpentier_pasc@hotmail.com>
2025-04-05 12:21:09 -0400

    Écriture de BakerCollector.Collect

--- GarbageCollection/Collectors/BakerCollector.cs
+++ GarbageCollection/Collectors/BakerCollector.cs
@@ -14,7 +14,9 @@ namespace GarbageCollection.Collectors
         private readonly EnvironmentMemory _memory;
         private Mutator _mutator ;
 
-        private 
+        private HashSet<int> _free ;
+        private HashSet<int> _unreached ;
+        
 
         public BakerCollector(EnvironmentMemory memory, Mutator mutator)
         {
@@ -33,7 +35,31 @@ namespace GarbageCollection.Collectors
 
         public void Collect()
         {
-            /* ------- À COMPLÉTER ------- */
+           HashSet<int> unscanned = new HashSet<int>(_unreached); ;
+           HashSet<int> scanned  = new HashSet<int>() ;
+           while (unscanned.Count > 0)
+           {
+               var current = unscanned.First() ;
+               unscanned.Remove(current) ;
+               scanned.Add(current) ;
+
+               CollectableObject? obj = null ; 
+               if (!_memory.TryDereference(current, out obj))
+               {
+                   throw new Exception($"Unable to dereference {current}") ;
+               }
+
+               foreach (var r in obj.References)
+               {
+                   if (_unreached.Contains(r))
+                   {
+                       _unreached.Remove(r) ;
+                       unscanned.Add(r) ; 
+                   }
+               } 
+           }
+           _free.UnionWith(_unreached) ;
+           _unreached  = scanned ;
         }
     }
 }
